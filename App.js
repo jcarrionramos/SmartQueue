6b1697ff02d7816bf7dac2f2a41e7c94de9befed
jcarrionramos <pollo_carrion@hotmail.com>
2018-11-04 14:27:15 -0300

    WIP

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {
-    createBottomTabNavigator
+    createMaterialTopTabNavigator
 } from 'react-navigation';
 
 import CameraScreen from './screens/camerascreen';
 import MapScreen from './screens/mapscreen';
 import QueueScreen from './screens/queuescreen';
 
-export default createBottomTabNavigator({
-    Mápa: MapScreen,
-    Cámara: CameraScreen,
-    Cola: QueueScreen,
+export default createMaterialTopTabNavigator({
+    Mapa: { screen: MapScreen },
+    Cámara: { screen: CameraScreen },
+    Cola: { screen: QueueScreen },
 });
